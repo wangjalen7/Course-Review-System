f842608df647444024529ad7af969dc5deff3019
Clark Pagels <Cjpagels2@gmail.com>
2023-04-25 13:25:21 -0400

    review exists + start of main.

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class main {
+    public void main(String args[]){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Welcome to course program! are you a new student or " +
+                "do you already have an account?");
+        System.out.println("Say n for new and e for existing");
+        String choice = myObj.nextLine();
+
+    }
+}
