8927d7a13a7b456befbe0429350f8de337d6ba1e
Clark Pagels <Cjpagels2@gmail.com>
2023-04-26 18:03:17 -0400

    review dealing functions.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class main {
             String pw = myObj.nextLine();
             if(password.equals(pw)){
                  student = new Student (username, password);
-                manager.addStudent(student);
+                 manager.addStudent(student);
 
             }
             else{
@@ -38,6 +38,31 @@ public class main {
                 System.out.println("either username or password is incorrect");
             }
         }
+        System.out.println("press 1 to exit, press 2 to select a class for reviewing, 3 for reading review,  4 for finding average numeric");
+        try {
+            Integer number = myObj.nextInt();
+            switch(number){
+                case 1:
+                    break;
+                case 2:
+                    System.out.println("what is course id?");
+                    String id = myObj.nextLine();
+                    System.out.println("what is course number?");
+                    String number = myObj.nextLine();
+                    addReview(id,number);
+                    break;
+                case 3:
+
+                default:
+                    System.out.println("wrong number");
+            }
+        }
+        }
+        catch{
+
+        }
+
+
 
     }
     public void checker(String choice){
@@ -46,6 +71,45 @@ public class main {
         }
 
     }
+    public Void getReviews(String a, String b){
+        Course course = new Course(a, b);
+        if(manager.newCourse(course) == false){
+            System.out.println("course is not in database");
+        }
+        else if(manager.getReviewsByCourse(course) == null){
+            System.out.println("no reviews");
+        }
+        else{
+            for(Review r :manager.getReviewsByCourse(course)){
+                System.out.println(r);
+            }
+        }
+
+    }
+    public Void getAverage(String a, String b){
+        Course course = new Course(a, b);
+        Review r = manager.getReviewsByCourse(course);
+        System.out.println(r.getRating());
+    }
 
 
+
+    public void addReview(String a, String b, Student student) {
+        Course course = new Course(a, b);
+        if (manager.newCourse(course) == false) {
+            manager.addCourse(course);
+        }
+        if (manager.reviewExists(course, student) == true {
+            System.out.println("review exists");
+        }
+        else{
+            Scanner scan = new Scanner(System.in);
+            System.out.println("What do you want to Say?");
+            String review = scan.nextLine();
+            System.out.println("What is your numeric review");
+            int score = scan.nextInt();
+            Review review = new Review(student, course, review, score);
+            manager.addReview(review);
+        }
+    }
 }
