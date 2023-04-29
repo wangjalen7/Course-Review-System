727ff17c34aab721e2e1ec203fadc2df7f7cf3d4
Jalen Wang <Jalen Wang>
2023-04-29 19:16:11 -0400

    working on write review gui

--- src/main/java/hw7/gui/reviewcontroller.java
+++ src/main/java/hw7/gui/reviewcontroller.java
@@ -1,4 +1,5 @@
 package hw7.gui;
+import hw7.menu.Course;
 import hw7.menu.ReviewMngr;
 import hw7.menu.Student;
 import javafx.fxml.FXMLLoader;
@@ -23,13 +24,55 @@ import javafx.stage.Stage;
 
 import java.security.Key;
 public class reviewcontroller {
+
+    public Label ratinglabel;
+    public TextField rating;
+    public Label commentlabel;
+    public TextField comment;
+    public Button Submit;
+    public TextField department;
+
+    public PasswordField number;
+    ReviewMngr manager = new ReviewMngr();
     @FXML
     private Button read = new Button();
-
+    public TextField usernameField = new TextField();
+    public TextField passwordField = new PasswordField();
+    public Label feedback = new Label();
 
     @FXML
     protected void write(){
-
+//        System.out.println("hw7.menu.Course:");
+//        myObj.nextLine();
+//        String core = myObj.nextLine();
+//        String d = core.substring(0,core.length()-4);
+//        String num = core.substring(core.length()-4);
+//        manager.chooseCourse(new Course(d,Integer.parseInt(num)));
+//        System.out.println("Enter rating(1-5):");
+//        String rate = myObj.nextLine();
+//        System.out.println("Add comment:");
+//        String msg = myObj.nextLine();
+//        try {
+//            manager.rate(msg, Integer.parseInt(rate));
+//            System.out.println("Done!");
+//        }
+//        catch (NoSuchElementException e) {
+//            System.out.println("You have already written a review for this hw7.menu.Course");
+//        }
+        ratinglabel.setVisible(true);
+        rating.setVisible(true);
+        commentlabel.setVisible(true);
+        comment.setVisible(true);
+        Submit.setVisible(true);
+        Student student = new Student(usernameField.getText(), passwordField.getText());
+        try {
+            manager.register(student);
+            feedback.setText("Registered and Logged In!");
+            feedback.setVisible(true);
+        } catch (NoSuchElementException e) {
+            feedback.setText("Username must be unique");
+            feedback.setVisible(true);
+        }
     }
 
     @FXML
