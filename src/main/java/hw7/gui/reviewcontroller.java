0f80e37128dd9a3fde1ae0cd83829c50c85fec93
Clark Pagels <Cjpagels2@gmail.com>
2023-04-29 21:11:14 -0400

    Start on making review and number think work. don't know how to work with buttons.

--- src/main/java/hw7/gui/reviewcontroller.java
+++ src/main/java/hw7/gui/reviewcontroller.java
@@ -1,5 +1,7 @@
 package hw7.gui;
+import hw7.menu.Review;
 import hw7.menu.ReviewMngr;
+import hw7.menu.Course;
 import hw7.menu.Student;
 import javafx.fxml.FXMLLoader;
 import javafx.application.Platform;
@@ -16,6 +18,7 @@ import javafx.scene.paint.Color;
 
 import java.io.IOException;
 import java.lang.Exception;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
@@ -23,9 +26,22 @@ import javafx.stage.Stage;
 
 import java.security.Key;
 public class reviewcontroller {
+    ReviewMngr manager = new ReviewMngr();
     @FXML
     private Button read = new Button();
+    @FXML
+    private Label reviews = new Label();
+    @FXML
+    private TextField department = new TextField();
 
+    @FXML
+    private TextField number = new TextField();
+
+    @FXML
+    private Button write = new Button();
+
+    @FXML
+    private Button Return = new Button();
 
     @FXML
     private TextField rating = new TextField();
@@ -62,5 +78,53 @@ public class reviewcontroller {
             e.printStackTrace();
         }
     }
+    @FXML
+    protected void setReview(){
+
+        try{
+            review.isVisible(true);
+            number.isVisible(true);
+            String message = review.getText();
+            Integer rating = Integer.parseInt(number.getText());
+            Course c = new Course(department.getText(), Integer.parseInt(number.getText()));
+            manager.chooseCourse(c);
+            manager.rate(message, rating);
+            review.clear();
+            number.clear();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    protected void getNumber(){
+        try{
+            Course c = new Course(department.getText(), Integer.parseInt(number.getText()));
+            manager.chooseCourse(c);
+            reviews.setText(manager.Average());
+        }
+
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    protected void getReview(){
+        try{
+            Course c = new Course(department.getText(), Integer.parseInt(number.getText()));
+            manager.chooseCourse(c);
+            ArrayList<String> messages= new ArrayList<>();
+            for(Review r: manager.output()){
+                messages.add(r.getMessage());
+            }
+            reviews.setText(String.valueOf(messages));
+        }
+
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
